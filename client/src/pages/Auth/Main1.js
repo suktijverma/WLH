a8bc8acfcdee8623e353df5afe05dcd6a99d2ec6
suktijverma <85821100+suktijverma@users.noreply.github.com>
2022-05-27 00:08:44 +0530

    Add files via upload

---
+++ client/src/pages/Auth/Main1.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main1 = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis tenetur illum expedita iste, neque nostrum ab. Neque cum commodi aliquam praesentium nesciunt voluptatem sit ipsum, dolores a omnis ea ad! Inventore soluta atque minima sit tempora error nostrum a sapiente at! Minima cumque quod, doloribus dolorem unde molestiae atque! Voluptates, velit maiores sunt non error animi ad? Hic ut delectus sapiente ab sequi culpa enim, officiis error, esse voluptate qui itaque laborum quidem, doloremque molestias voluptatem! Quos, corrupti voluptatibus maxime quis velit debitis recusandae rerum nobis voluptatem autem in cupiditate eaque consectetur esse hic. Labore ullam nam et voluptatum odio perferendis numquam quaerat tempora nemo quos quidem, ut ea maxime ex minus dolor adipisci laboriosam libero nisi saepe omnis. Provident molestias perferendis laudantium voluptas, natus exercitationem iure aspernatur consequatur quidem aut vero in amet assumenda? Fugiat, beatae. Magni dolores, fugiat alias corrupti explicabo dolorum porro maxime odio eaque? Obcaecati distinctio tempora blanditiis facilis porro consequatur omnis voluptatem magni laudantium excepturi repudiandae dolorem, culpa impedit iusto neque debitis laborum sequi nemo architecto aut. Iure totam dolor reprehenderit voluptas repudiandae suscipit? Quod ipsum perspiciatis omnis expedita ut ipsa, eligendi molestiae recusandae vel maxime consectetur voluptatum libero odio nemo deserunt numquam dolorem id excepturi quidem! Reprehenderit eveniet eligendi id alias sint cum asperiores mollitia deserunt dolorem molestiae repellendus dolorum, minima maxime ipsam. Nisi consequuntur ea molestiae.
+    </div>
+  )
+}
+
+export default Main1
